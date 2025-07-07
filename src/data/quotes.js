71d6082ce2cdb608dd511f67af2972b43b81e56b
mtaha-23 <muhammadtaha2723@gmail.com>
2025-07-07 14:53:09 +0500

    feat: complete assignment 1

---
+++ src/data/quotes.js
@@ -0,0 +1,8 @@
+export const quotes = [
+  { topic: "life", text: "Life is what happens when you're busy making other plans." },
+  { topic: "life", text: "To live is the rarest thing in the world. Most people exist, that is all." },
+  { topic: "life", text: "Get busy living or get busy dying." },
+  { topic: "success", text: "Success is not the key to happiness. Happiness is the key to success." },
+  { topic: "success", text: "Success usually comes to those who are too busy to be looking for it." },
+  { topic: "success", text: "Don’t be afraid to give up the good to go for the great." }
+]
